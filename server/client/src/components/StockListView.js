7ad9fcf92d0f5c7df85f48f28ce664ff49e99a68
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-12-12 08:14:23 -0500

    styling card

--- server/client/src/components/StockListView.js
+++ server/client/src/components/StockListView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './../App.css';
-
+import './../testcss.css';
+// import './../App.css';
 // import newdata from './stock_data.json';
 // var jsonString = JSON.stringify(newdata);
 
@@ -21,28 +21,14 @@ export default class StockListView extends Component {
   //   }
 
 // }
-// "ticker": "PIH",
-//     "company_name": "1347 Property Insurance Holding",
-//     "market_cap": 29497118.000000004,
-//     "52_week_delta": -0.0020661354,
-//     "dividend_payout_ratio": 0,
-//     "forward_eps": 1.15,
-//     "g_revenue": 0.749,
-//     "gross_margin_pct": 0.27115,
-//     "ebitda_margin_pct": 0.0054400004,
-//     "price_to_forward_earnings": 4.261,
-//     "price_to_book": 0.77250516,
-//     "debt_to_equity": 0,
-//     "return_on_assets": 0.00078,
-//     "return_on_equity": 0.025150001,
-//     "largest_institutional_owner": "CWA Asset Management Group, LLC",
-//     "recommendation": "none"
 
   render() {
     return(
-      <div className="stock-list-view">
-        {this.ListItems()}
-      </div>
+   
+      // <div className="stock-list-view">
+        this.ListItems()
+      // </div>
+    
     )
   }
 
@@ -58,18 +44,30 @@ export default class StockListView extends Component {
 
     }
   {
-    return this.props.stocks.map((item, index) => {
-      return(
-        <div className="stock-item" key={item}>
-          <h1 className="stock-title"> {item} </h1>
-          <p className="stock-description"> Overall Risk : <b>Low </b> </p> Beta: {item}, PEG: {newData.EPS}, P/S: .67 
-          <p className="stock-description"> </p> PE Ratio: 1.2, SD: 0.24, WACC: .52 
-          {/* <button id={index} name={item} className="stock-btn-info" onClick={this.props.ModalExample}> Risk info </button> */}
-          <button id={index} name={item} className="stock-btn-delete" onClick={this.props.onDelete}> x </button>
-        </div>
+    // return this.props.stocks.map((item, index) => {
+    //   return(
+    //   //  <div class="wrapper">
+    //       <div className="stock-item" key={item}>
+    //         <h1 className="stock-title"> {item} </h1>
+    //         <p className="stock-description"> Overall Risk : <b>Low </b> </p> Beta: {item}, PEG: {newData.EPS}, P/S: .67 
+    //         <p className="stock-description"> </p> PE Ratio: 1.2, SD: 0.24, WACC: .52 
+    //         {/* <button id={index} name={item} className="stock-btn-info" onClick={this.props.ModalExample}> Risk info </button> */}
+    //         <button id={index} name={item} className="stock-btn-delete" onClick={this.props.onDelete}> x </button>
+    //       </div>
+
+
+      // return this.props.stocks.map((item, index) => {
+      //   return(
+        
+        
       )
     })
+    }
+    }
   }
-}
-}
+
+
+
+
+
 
